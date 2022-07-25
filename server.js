cf598d34528a3ad26dd6b84f9d6db5f89a0450b2
timcs <timcs1274@gmail.com>
2022-07-25 10:28:20 -0400

    added create note function

--- server.js
+++ server.js
@@ -4,3 +4,37 @@ const path = require('path')
 const express = require('express');
 const app = express();
 const notes = require('./Develop/db/db.json');
+
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//adding middleware
+
+
+
+//function to create note
+function createNote(body, noteArr) {
+    const newNote = body;
+    
+    if (!Array.isArray(noteArr))
+    noteArr = [];
+
+    if(noteArr.length === 0)
+    noteArr.push(0);
+
+    body.id = noteArr[0];
+    noteArr[0]++;
+
+    return createNote;
+}
+
+
+
+//function to delete note
+
+
+
+//add server function
